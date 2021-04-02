d7af25fc9157d4afa23e878f594226747ca22c13
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-02 21:55:33 +0545

    function to get test case by id

--- models/cases_model.go
+++ models/cases_model.go
@@ -35,3 +35,22 @@ func GetCaseByTitle(title string) (TestCaseModel, error) {
 	}
 	return tc, nil
 }
+
+func GetCaseByID(id uint32) (TestCaseModel, error) {
+	conn := Connect()
+	defer conn.Close()
+	sql := `SELECT * FROM test_case WHERE id=$1`
+	rs, err := conn.Query(sql, id)
+	if err != nil {
+		return TestCaseModel{}, err
+	}
+	defer rs.Close()
+	var tc TestCaseModel
+	if rs.Next() {
+		err := rs.Scan(&tc.ID,&tc.Title,&tc.Date,&tc.TestedBy,&tc.Functionality,&tc.Summary,&tc.Description,&tc.Data,&tc.URL,&tc.ExpectedResult,&tc.ActualResult,&tc.Environment,&tc.Device)
+		if err != nil {
+			return TestCaseModel{}, err
+		}
+	}
+	return tc, nil
+}
