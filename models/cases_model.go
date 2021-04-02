421597d6603e84894f0f66cf6f762099566a32ad
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-02 21:55:02 +0545

    function to get test case by given title

--- models/cases_model.go
+++ models/cases_model.go
@@ -17,3 +17,21 @@ type TestCaseModel struct {
 }
 
 
+func GetCaseByTitle(title string) (TestCaseModel, error) {
+	conn := Connect()
+	defer conn.Close()
+	sql := `SELECT * FROM test_case WHERE title=$1`
+	rs, err := conn.Query(sql, title)
+	if err != nil {
+		return TestCaseModel{}, err
+	}
+	defer rs.Close()
+	var tc TestCaseModel
+	if rs.Next() {
+		err := rs.Scan(&tc.ID,&tc.Title,&tc.Date,&tc.TestedBy,&tc.Functionality,&tc.Summary,&tc.Description,&tc.Data,&tc.URL,&tc.ExpectedResult,&tc.ActualResult,&tc.Environment,&tc.Device)
+		if err != nil {
+			return TestCaseModel{}, err
+		}
+	}
+	return tc, nil
+}
