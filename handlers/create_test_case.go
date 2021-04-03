b0272a4004e96cdb4b262af79580fb792a572e43
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-03 23:58:31 +0545

    decoded json logger added

--- handlers/create_test_case.go
+++ handlers/create_test_case.go
@@ -17,6 +17,7 @@ func createTestCase(w http.ResponseWriter, r *http.Request) {
 		log.Println(err.Error())
 		return
 	}
+	log.Printf("%+v",testCase)
 	conn := models.Connect()
 	createTestCaseQuery := `INSERT INTO test_cases (title, date, tested_by, functionality, summary, description, data, url, expected_result, actual_result, environment, device) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)`
 	_, err = conn.Exec(createTestCaseQuery, testCase.Title, testCase.Date, testCase.TestedBy, testCase.Functionality,testCase.Summary,testCase.Description,testCase.Data,testCase.URL,testCase.ExpectedResult,testCase.ActualResult,testCase.Environment,testCase.Device)
