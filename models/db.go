cfb86024ae5972f1bb3732ec9c2ec42aa2d9463a
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-08 10:41:37 +0545

    function to create tables if not already created

--- models/db.go
+++ models/db.go
@@ -2,7 +2,7 @@ package models
 
 import(
 	"fmt"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"database/sql"
 	_ "github.com/lib/pq"
 	"os"
@@ -22,3 +22,45 @@ func Connect() *sql.DB {
 	}
 	return db
 }
+
+func CreateTablesIfNotExists(db *sql.DB) error {
+	//Create table test_cases if not already exists.
+	_, err := db.Exec(casesModelQuery)
+	if err != nil {
+		log.Errorln(err)
+		return err
+	}
+
+	//create user model
+	_, err = db.Exec(usersModelQuery)
+	if err != nil {
+		log.Errorln(err)
+		return err
+	}
+	return nil
+}
+
+var casesModelQuery = `CREATE TABLE IF NOT EXISTS test_cases (
+    id BIGSERIAL PRIMARY KEY ,
+    title TEXT NOT NULL,
+    date DATE NOT NULL,
+    tested_by VARCHAR (50) NOT NULL,
+    functionality VARCHAR(50) NOT NULL,
+    summary TEXT NOT NULL,
+    description TEXT,
+    data TEXT,
+    url VARCHAR(255),
+    expected_result TEXT NOT NULL,
+    actual_result TEXT NOT NULL,
+    environment VARCHAR(55) NOT NULL,
+    device VARCHAR(50)
+);`
+
+var usersModelQuery = `CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(35) NOT NULL,
+    email VARCHAR(320) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    created_at DATE NOT NULL,
+    superuser BOOLEAN NOT NULL DEFAULT FALSE
+);`
