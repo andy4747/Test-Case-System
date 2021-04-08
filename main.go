4197b7684f3a433092bd2eb22f5a2af540e85cc0
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-08 10:42:00 +0545

    logger changed

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"github.com/angeldhakal/testcase-ms/handlers"
-	"log"
+	"github.com/angeldhakal/testcase-ms/models"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func main() {
+	err := models.CreateTablesIfNotExists(models.Connect())
+	if err != nil {
+		log.Fatalln(err)
+	}
 	log.Println("Listening at http://localhost:8080/")
+	log.Infoln("Necessary tables are present.")
 
-	r := handlers.NewRouter()
-	err := http.ListenAndServe(":8080", r)
+	r := handlers.MainRouter()
+	err = http.ListenAndServe(":8080", r)
 
 	if err != nil {
 		log.Fatal(err.Error())
