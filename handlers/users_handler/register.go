56c403ca75e7302aad5dce2d8909b6c47bd80382
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-08 10:42:50 +0545

    handle function for register endpoint

--- handlers/users_handler/register.go
+++ handlers/users_handler/register.go
@@ -1,12 +1,23 @@
 package users_handler
 
-
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/angeldhakal/testcase-ms/models"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
-func registerUser(w http.ResponseWriter, r *http.Request) {
+func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type","application/json")
-	fmt.Fprintf(w,`{"endpoint":"register"}`)
+	var id uint64 = 0
+	var user models.Users
+	user.ID = id
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		log.Errorln(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 }
+
