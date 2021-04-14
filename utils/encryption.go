fc277d7469e5d6fb6e9b45a3c7d47bf3250f9c9c
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-14 12:35:26 +0545

    method to hash password and compare hash password created

--- utils/encryption.go
+++ utils/encryption.go
@@ -1,3 +1,21 @@
 package utils
 
+import (
+    "golang.org/x/crypto/bcrypt"
+    log "github.com/sirupsen/logrus"
+)
 
+func HashPassword(password *string) {
+    bytePass := []byte(*password)
+    hashedPassword, _ := bcrypt.GenerateFromPassword(bytePass, bcrypt.DefaultCost)
+    *password = string(hashedPassword)
+}
+
+func ComparePassword(hashedPassword, password string){
+    hash := []byte(hashedPassword)
+    pass := []byte(password)
+    err := bcrypt.CompareHashAndPassword(hash, pass)
+    if err != nil {
+        log.Errorln("Couldn't Compare Hash and Password")
+    }
+}
