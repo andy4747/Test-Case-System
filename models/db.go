5b18770cfa60178086264e6eb7b2ae0c7d1e51ac
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-14 12:34:28 +0545

    setup db connection using gorm

--- models/db.go
+++ models/db.go
@@ -1,11 +1,13 @@
 package models
 
-import(
+import (
 	"fmt"
-	log "github.com/sirupsen/logrus"
-	"database/sql"
-	_ "github.com/lib/pq"
 	"os"
+
+	_ "github.com/lib/pq"
+	log "github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var (
@@ -14,53 +16,17 @@ var (
 	dbPassword = os.Getenv("DB_PASSWORD")
 )
 
-func Connect() *sql.DB {
-	postgresConfig := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",dbUser,dbPassword,dbName)
-	db, err := sql.Open("postgres", postgresConfig)
+func Connect() *gorm.DB {
+	postgresURI := fmt.Sprintf("host=localhost user=%s dbname=%s sslmode=disable password=%s", dbUser, dbName, dbPassword)
+	db, err := gorm.Open(postgres.Open(postgresURI))
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Errorln(err)
+		panic("Connection to database failed")
+	}
+	err = db.AutoMigrate(&Users{}, &TestCaseModel{})
+	if err != nil {
+		log.Errorln(err)
+		panic("Migration Failed")
 	}
 	return db
 }
-
-func CreateTablesIfNotExists(db *sql.DB) error {
-	//Create table test_cases if not already exists.
-	_, err := db.Exec(casesModelQuery)
-	if err != nil {
-		log.Errorln(err)
-		return err
-	}
-
-	//create user model
-	_, err = db.Exec(usersModelQuery)
-	if err != nil {
-		log.Errorln(err)
-		return err
-	}
-	return nil
-}
-
-var casesModelQuery = `CREATE TABLE IF NOT EXISTS test_cases (
-    id BIGSERIAL PRIMARY KEY ,
-    title TEXT NOT NULL,
-    date DATE NOT NULL,
-    tested_by VARCHAR (50) NOT NULL,
-    functionality VARCHAR(50) NOT NULL,
-    summary TEXT NOT NULL,
-    description TEXT,
-    data TEXT,
-    url VARCHAR(255),
-    expected_result TEXT NOT NULL,
-    actual_result TEXT NOT NULL,
-    environment VARCHAR(55) NOT NULL,
-    device VARCHAR(50)
-);`
-
-var usersModelQuery = `CREATE TABLE IF NOT EXISTS users (
-    id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(35) NOT NULL,
-    email VARCHAR(320) UNIQUE NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    created_at DATE NOT NULL,
-    superuser BOOLEAN NOT NULL DEFAULT FALSE
-);`
