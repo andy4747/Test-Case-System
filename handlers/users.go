c716e481cfb030620b11a339b11c8819cc8e82b1
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-15 19:30:16 +0545

    handler func to delete a user added

---
+++ handlers/users.go
@@ -0,0 +1,136 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/angeldhakal/testcase-ms/models"
+	"github.com/angeldhakal/testcase-ms/repository"
+	"github.com/angeldhakal/testcase-ms/utils"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+)
+
+type UserHandler interface {
+	AddUser(http.HandlerFunc)
+	GetUser(http.HandlerFunc)
+	GetAllUser(http.HandlerFunc)
+	UpdateUser(http.HandlerFunc)
+	DeleteUser(http.HandlerFunc)
+	SignInUser(http.HandlerFunc)
+}
+
+type userHandler struct {
+	repo repository.UserRepository
+}
+
+func NewUserHandler() UserHandler {
+	return &userHandler{
+		repo: repository.NewUserRepository(),
+	}
+}
+
+func (h *userHandler) AddUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user models.Users
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		log.Errorln("Invalid Request")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	utils.HashPassword(&user.Password)
+	user, err = h.repo.AddUser(user)
+	if err != nil{
+		log.Errorln("Couldn't Register user due to some server issues")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	user.Password = ""
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *userHandler) GetUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var user models.Users
+	// parsing id parameter from url endpoint to integer value
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		log.Warnln("Enter valid user id")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	user, err = h.repo.GetUser(id)
+	if err != nil {
+		log.Warnln("Couldn't get user due to some server issue")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	var userJson []byte
+	userJson, err = json.MarshalIndent(&user, "", "  ")
+	if err != nil {
+		log.Errorln("Couldn't encode json due to some server issues")
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(userJson)
+}
+
+func (h *userHandler) GetAllUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var users []models.Users
+	users, err := h.repo.GetAllUsers()
+	if err != nil {
+		log.Errorln("Couldn't get users due to some server issues")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	var allUsers []byte
+	allUsers, err = json.MarshalIndent(&users, "", "  ")
+	if err != nil {
+        log.Errorln("Could't encode the json due to some server issue")
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+	}
+    w.Write(allUsers)
+}
+
+func (h *userHandler) UpdateUser(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type","application/json")
+    var user models.Users
+    err := json.NewDecoder(r.Body).Decode(&user)
+    if err != nil {
+        log.Errorln("Provide valid information")
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    user, err = h.repo.UpdateUser(user)
+    if err != nil {
+        log.Errorln("Couldn't update due to some server issues")
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    w.WriteHeader(http.StatusOK)
+}
+
+func (h *userHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    params := mux.Vars(r)
+    id, err := strconv.Atoi(params["id"])
+    if err != nil {
+        log.Errorln("Enter valid user id")
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    var user models.Users
+    user.ID = uint(id)
+    user, err = h.repo.DeleteUser(user)
+    if err != nil {
+        log.Errorln("Couldn't delete user due to some server error")
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    w.WriteHeader(http.StatusNoContent)
+}
