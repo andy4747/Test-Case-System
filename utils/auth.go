b17b2c3b5da81b3cdc6c588cbed53a2dc01b3da1
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-17 12:51:24 +0545

    function to generate auth token and validate a token

--- utils/auth.go
+++ utils/auth.go
@@ -1,7 +1,35 @@
 package utils
 
+import (
+	"os"
+	"time"
+	"github.com/dgrijalva/jwt-go"
+    "fmt"
+)
 
 type Credentials struct {
     Email string	`json:"email"`
 	Password string	`json:"password"`
 }
+
+func GenerateToken(email string) string {
+    claims := jwt.MapClaims{
+        "exp" : time.Now().Add(time.Hour * 3).Unix(),
+        "iat" : time.Now().Unix(),
+        "user": email,
+    }
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    t, _ := token.SignedString(os.Getenv("SECRET_KEY"))
+    return t
+}
+
+func ValidateToken(token string) (*jwt.Token, error) {
+    return jwt.Parse(token, func (token *jwt.Token) (interface{}, error){
+        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+            //return nil as secret key
+            return nil, fmt.Errorf("wring signing method: %v",token.Header["alg"])
+        }
+        return []byte(os.Getenv("SECRET_KEY")), nil
+    })
+}
+
