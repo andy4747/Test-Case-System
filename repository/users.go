f35c95a0baa79f84d5008aef6810bcfa7fe33697
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-28 15:42:17 +0545

    missing Delete method added to DeleteUser method

--- repository/users.go
+++ repository/users.go
@@ -1,8 +1,8 @@
 package repository
 
 import (
-    "github.com/angeldhakal/testcase-ms/models"
-    "gorm.io/gorm"
+	"github.com/angeldhakal/testcase-ms/models"
+	"gorm.io/gorm"
 )
 
 type UserRepository interface {
@@ -29,29 +29,27 @@ func (db *userRepository) AddUser(user models.Users) (models.Users, error) {
 }
 
 func (db *userRepository) GetUser(id int) (user models.Users, err error) {
-    return user, db.connection.First(&user, id).Error
+	return user, db.connection.First(&user, id).Error
 }
 
-
 func (db *userRepository) GetUserByEmail(email string) (user models.Users, err error) {
-    return user, db.connection.First(&user, "email=?", email).Error
+	return user, db.connection.First(&user, "email=?", email).Error
 }
 
 func (db *userRepository) GetAllUsers() (users []models.Users, err error) {
-    return users, db.connection.Find(&users).Error
+	return users, db.connection.Find(&users).Error
 }
 
 func (db *userRepository) UpdateUser(user models.Users) (models.Users, error) {
-    if err := db.connection.First(&user, user.ID).Error; err != nil {
-        return user, err
-    }
-    return user, db.connection.Model(&user).Updates(&user).Error
+	if err := db.connection.First(&user, user.ID).Error; err != nil {
+		return user, err
+	}
+	return user, db.connection.Model(&user).Updates(&user).Error
 }
 
 func (db *userRepository) DeleteUser(user models.Users) (models.Users, error) {
-    if err := db.connection.First(&user, user.ID).Error; err != nil {
-        return user, err
-    }
-    return user, db.connection.Model(&user).Error
+	if err := db.connection.First(&user, user.ID).Error; err != nil {
+		return user, err
+	}
+	return user, db.connection.Delete(&user).Error
 }
-
