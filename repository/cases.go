8b02a6003197f0c369e510ad63fe3e6473d1e66f
AngelDhakal <anjaldhakal01@gmail.com>
2021-04-28 15:43:54 +0545

    all the method of CaseRepository interface implemented

--- repository/cases.go
+++ repository/cases.go
@@ -6,31 +6,48 @@ import (
 )
 
 type CaseRepository interface {
-    GetCase(int) (models.TestCaseModel, error)
-    GetAllCases() ([]models.TestCaseModel, error)
-    AddCase(models.TestCaseModel) (models.TestCaseModel, error)
-    UpdateCase(models.TestCaseModel) (models.TestCaseModel, error)
-    DeleteCase(models.TestCaseModel) (models.TestCaseModel, error)
+	GetCase(int) (models.TestCaseModel, error)
+	GetAllCases(models.TestCaseModel) ([]models.TestCaseModel, error)
+	AddCase(models.TestCaseModel) (models.TestCaseModel, error)
+	UpdateCase(models.TestCaseModel) (models.TestCaseModel, error)
+	DeleteCase(models.TestCaseModel) (models.TestCaseModel, error)
 }
 
 type caseRepository struct {
-    connection *gorm.DB
+	connection *gorm.DB
 }
 
 func NewCaseRepository() *caseRepository {
-    return &caseRepository{
-        connection: models.Connect(),
-    }
+	return &caseRepository{
+		connection: models.Connect(),
+	}
 }
 
+func (db *caseRepository) GetCase(id int, user string) (testCase models.TestCaseModel, err error) {
+	if err := db.connection.Where(&testCase, id).Error; err != nil {
+		return testCase, err
+	}
+	return testCase, db.connection.Where("id=? and user=?", id, user).Error
+}
 
-func (db *caseRepository) GetCase(id int) (models.TestCaseModel, error) {return models.TestCaseModel{}, nil}
+func (db *caseRepository) GetAllCases(user string) (cases []models.TestCaseModel, err error) {
+	return cases, db.connection.Where("user=?", user).Find(&cases).Error
+}
 
-func (db *caseRepository) GetAllCases() ([]models.TestCaseModel, error)  {return []models.TestCaseModel{}, nil}
+func (db *caseRepository) AddCase(testCase models.TestCaseModel) (models.TestCaseModel, error) {
+	return testCase, db.connection.Create(&testCase).Error
+}
 
-func (db *caseRepository) AddCase(testCase models.TestCaseModel) (models.TestCaseModel, error) {return models.TestCaseModel{}, nil}
+func (db *caseRepository) UpdateCase(testCase models.TestCaseModel) (models.TestCaseModel, error) {
+	if err := db.connection.First(&testCase, testCase.ID).Error; err != nil {
+		return testCase, err
+	}
+	return testCase, db.connection.Model(&testCase).Updates(&testCase).Error
+}
 
-func (db *caseRepository) UpdateCase(testCase models.TestCaseModel) (models.TestCaseModel, error)  {return models.TestCaseModel{}, nil}
-
-func (db *caseRepository) DeleteCase(testCase models.TestCaseModel) (models.TestCaseModel, error)  {return models.TestCaseModel{}, nil}
-
+func (db *caseRepository) DeleteCase(testCase models.TestCaseModel) (models.TestCaseModel, error) {
+	if err := db.connection.First(&testCase, testCase.ID).Error; err != nil {
+		return testCase, err
+	}
+	return testCase, db.connection.Delete(&testCase).Error
+}
